6162a6bafb3744cf17b90142f76812eb99788643
Henry Li <henry1943@163.com>
2023-08-12 14:20:18 +0800

    fix: bugfix

--- src/agents/template-agent.ts
+++ src/agents/template-agent.ts
@@ -13,7 +13,7 @@ export async function renderTemplate(project: Project, sourceFileName: string, t
   await ensureDirectoryExists(targetDir);
   const templateContent = await fs.readFile(templatePath, 'utf-8');
   const renderedContent = templateContent.replace(/{{\s*([a-zA-Z0-9_]+)\s*}}/g, (_, key: keyof Project) => {
-    return project[key];
+    return project[key] ? project[key].toString() : '';
   });
   await fs.writeFile(targetPath, renderedContent);
 }
